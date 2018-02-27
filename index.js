3c5f1617e8323e6a485e1caa16ceceb623039de2
Joe Greiner <jrgreiner115@gmail.com>
2018-02-27 01:34:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function whisper(string) {
 function logShout(string) {
   return console.log(`HELLO`)
 }
-function whisperShout(string) {
+function logWhisper(string) {
   return console.log(`HELLO`)
 }
