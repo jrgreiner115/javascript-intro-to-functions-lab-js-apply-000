b74bca6458dd641035dff5104731ec05db0c350d
Joe Greiner <jrgreiner115@gmail.com>
2018-02-27 01:37:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) return `I can't hear you!`;
   else (string === string.toUpperCase()) return `YES INDEED!`
-  else (string === string) return `I love you, too.`
+  else (string === `I love you, Grandma.`) return `I love you, too.`
 }
